f5375058d34d74c6c22e54143da9c6ac80568b03
Dominiciss <dominichmartinezmanuel@gmail.com>
2025-06-04 22:09:21 -0300

    Made validations for the creation form

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
 const params = new URLSearchParams(window.location.search);
 params.forEach((value, key) => {
-    console.log(`${key}: ${value}`);
+    console.log(`${key}: ${atob(value)}`);
 })
--- js/register-pasapalabra.js
+++ js/register-pasapalabra.js
@@ -8,7 +8,7 @@ divLetters.forEach((letter, index) => {
     letter.addEventListener("click", () => {
         var register = pasapalabra.querySelector(".register")
 
-        divLetters.forEach((letter_) => {letter_.style.backgroundColor = "transparent"})
+        divLetters.forEach((letter_) => { letter_.style.backgroundColor = "transparent" })
 
         register.querySelector(".word").value = (letter.getAttribute("word") == null) ? "" : letter.getAttribute("word")
         register.querySelector(".hint").value = (letter.getAttribute("hint") == null) ? "" : letter.getAttribute("hint")
@@ -25,8 +25,15 @@ divLetters.forEach((letter, index) => {
     circleContainer.replaceChild(letter, circleContainer.children[index])
 })
 
+pasapalabra.querySelector(".register > .return").addEventListener("click", () => {
+    divLetters.forEach((letter_) => { letter_.style.backgroundColor = "transparent" })
+
+    pasapalabra.querySelector(".title").setAttribute("hidden", "false")
+    pasapalabra.querySelector(".register").setAttribute("hidden", "true")
+})
+
 pasapalabra.querySelector(".register > .submit").addEventListener("click", () => {
-    divLetters.forEach((letter_) => {letter_.style.backgroundColor = "transparent"})
+    divLetters.forEach((letter_) => { letter_.style.backgroundColor = "transparent" })
 
     var register = pasapalabra.querySelector(".register")
 
@@ -38,17 +45,60 @@ pasapalabra.querySelector(".register > .submit").addEventListener("click", () =>
 
     letterSaved.setAttribute("word", word)
     letterSaved.setAttribute("hint", hint)
+    if ((word != null & word != "") & (hint != null & hint != "")) {
+        letterSaved.setAttribute("incomplete", "false")
 
-    circleContainer.replaceChild(letterSaved, circleContainer.children[indexSaved])
+        circleContainer.replaceChild(letterSaved, circleContainer.children[indexSaved])
+    } else {
+        letterSaved.setAttribute("incomplete", "true")
+    }
+
+    checkSubmit()
 })
 
+pasapalabra.querySelector(".game-title").addEventListener("input", () => {
+    var input = pasapalabra.querySelector(".game-title")
 
+    if (input.value != null & input.value != "") {
+        input.setAttribute("incomplete", "false")
+    } else {
+        input.setAttribute("incomplete", "true")
+    }
+
+    checkSubmit()
+})
+
+// WORK IN PROGRESS
 document.querySelector(".create").addEventListener("click", () => {
-    getRequest = ""
+    getRequest = `title=${pasapalabra.querySelector(".game-title").value}&`
     divLetters.forEach((letter, index) => {
         var word = letter.getAttribute("word")
-        var hint = letter.getAttribute("hint") 
+        var hint = letter.getAttribute("hint")
         getRequest += `letter${index}=["${(word == null) ? "" : word}","${(hint == null) ? "" : hint}"]&`
     })
     window.location = "./pages/game.html?" + getRequest
 })
+
+function checkSubmit() {
+    var valid = true
+    var input = pasapalabra.querySelector(".game-title")
+
+    divLetters.forEach((letter, index) => {
+        var word = letter.getAttribute("word")
+        var hint = letter.getAttribute("hint")
+
+        if ((word == null || word == "") || (hint == null || hint == "")) {
+            valid = false
+        }
+    })
+
+    if (input.value == null || input.value == "") {
+        valid = false
+    }
+
+    if (valid) {
+        document.querySelector(".create").setAttribute("disabled", "false")
+    } else {
+        document.querySelector(".create").setAttribute("disabled", "true")
+    }
+}
