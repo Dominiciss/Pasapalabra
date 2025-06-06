c13f208dc1c64ecb4afeb5130347f03473ee0d4f
Dominiciss <dominichmartinezmanuel@gmail.com>
2025-06-06 15:02:33 -0300

    Added connection in the file "create-game.php"

--- php/create-game.php
+++ php/create-game.php
@@ -4,5 +4,8 @@ include_once "./connection.php";
 $title = $_POST["title"]
 $letters = $_POST["letters"]
 
-echo $title
-echo $letters
+$connect = new Connection();
+
+$result = $connect->updateQuery("insert into pasapalabra (name, letters) VALUES ('$name', '$letters')");
+
+echo $result;
